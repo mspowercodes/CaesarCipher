ce15e872942b2e263a2c5ddd01486e1cb438b298
mspowercodes <dpower@isbos.org>
2026-08-16 19:53:02 -0400

    Fix decryption function and update user input handling

--- Problem7/app_p7.py
+++ Problem7/app_p7.py
@@ -1,7 +1,12 @@
 import streamlit as st
 import string
 
-def caesar_shift3(message):
+st.title("Problem 7 App")
+
+# Get user input
+shift = st.slider("Enter the shift amount:", 0, 25)
+
+def caesar_decrypt_user(message):
     table = str.maketrans(string.ascii_uppercase[3:26]+string.ascii_uppercase[0:3], string.ascii_lowercase)
     return message.translate(table)
 
@@ -11,6 +16,6 @@ st.title("Problem 7 App")
 user_message = st.text_input("Enter a plaintext message:")
 
 if user_message:
-  shifted_message = caesar_shift3(user_message)
+  shifted_message = caesar_decrypt_user(user_message)
   st.subheader("Encrypted message:")
   st.write(shifted_message)
