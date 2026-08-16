42ea8521f55c56a79cd4f7f528712d988060de7d
mspowercodes <dpower@isbos.org>
2026-08-16 16:56:37 -0400

    Update app_p5.py

--- Problem5/app_p5.py
+++ Problem5/app_p5.py
@@ -3,3 +3,13 @@ import string
 def caesar_shift3(message):
     table = str.maketrans(string.ascii_lowercase, string.ascii_uppercase)
     return message.translate(table)
+
+st.title("Problem 5 App")
+
+# Get user input
+user_message = st.text_input("Enter a plaintext message:")
+
+if user_message:
+  shifted_message = caesar_shift3(user_message)
+  st.subheader("Encrypted message:")
+  st.write(shifted_message)
