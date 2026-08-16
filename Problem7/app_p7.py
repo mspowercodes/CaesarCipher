8e9b56fd278a984f74c536ec46dceb676ee4655d
mspowercodes <dpower@isbos.org>
2026-08-16 19:51:23 -0400

    Update and rename app_p5d.py to app_p7.py

--- Problem7/app_p7.py
+++ Problem7/app_p7.py
@@ -2,10 +2,10 @@ import streamlit as st
 import string
 
 def caesar_shift3(message):
-    table = str.maketrans(string.ascii_lowercase, string.ascii_uppercase[3:26]+string.ascii_uppercase[0:3])
+    table = str.maketrans(string.ascii_uppercase[3:26]+string.ascii_uppercase[0:3], string.ascii_lowercase)
     return message.translate(table)
 
-st.title("Problem 5d App")
+st.title("Problem 7 App")
 
 # Get user input
 user_message = st.text_input("Enter a plaintext message:")
