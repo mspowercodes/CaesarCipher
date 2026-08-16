ee1caf2b631e0c9996e880e9f8b0e89a17c09446
mspowercodes <dpower@isbos.org>
2026-08-16 18:19:01 -0400

    Update and rename Problem 5e to app_p5e.py

---
+++ app_p5e.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import string
+
+shift = int(input(“Enter the shift amount:”))
+
+def caesar_shift_user(message):
+    table = str.maketrans(string.ascii_lowercase, string.ascii_uppercase[shift:26]+string.ascii_uppercase[0,shift])
+    return message.translate(table)
+
+st.title("Problem 5e App")
+
+# Get user input
+user_message = st.text_input("Enter a plaintext message:")
+
+if user_message:
+  shifted_message = caesar_shift_user(user_message)
+  st.subheader("Encrypted message:")
+  st.write(shifted_message)
