7b26239ef1336825efc3b96443690fd671f7c501
mspowercodes <dpower@isbos.org>
2026-08-16 16:28:35 -0400

    Update app_p3.py

--- Problem3/app_p3.py
+++ Problem3/app_p3.py
@@ -4,12 +4,12 @@ def caesar_shift3(message):
   table = str.maketrans("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ", "DEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABC")
   return message.translate(table)
 
-st.title("Caesar Cipher App")
+st.title("The App")
 
 # Get user input
-user_message = st.text_input("Enter a message to shift:")
+user_message = st.text_input("Enter a plaintext message:")
 
 if user_message:
   shifted_message = caesar_shift3(user_message)
-  st.subheader("Shifted Result:")
+  st.subheader("Encrypted message:")
   st.write(shifted_message)
