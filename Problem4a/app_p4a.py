7791e6d6c21772800ccd256d6cb9c9e3678edd5c
mspowercodes <dpower@isbos.org>
2026-08-16 16:50:18 -0400

    Update app_p4a.py

--- Problem4a/app_p4a.py
+++ Problem4a/app_p4a.py
@@ -1,15 +1,15 @@
 import streamlit as st
 
-def caesar_shift3(message):
-  table = str.maketrans("abcdefghijklmnopqrstuvwxyz", "DEFGHIJKLMNOPQRSTUVWXYZABC")
+def caesar_shift4(message):
+  table = str.maketrans("abcdefghijklmnopqrstuvwxyz", "EFGHIJKLMNOPQRSTUVWXYZABCD")
   return message.translate(table)
 
-st.title("Problems 1-2 App")
+st.title("Problem 4a App")
 
 # Get user input
 user_message = st.text_input("Enter a plaintext message:")
 
 if user_message:
-  shifted_message = caesar_shift3(user_message)
+  shifted_message = caesar_shift4(user_message)
   st.subheader("Encrypted message:")
   st.write(shifted_message)
