c8661bde06274435d8593bdde5f0aefe4d0e04a5
mspowercodes <dpower@isbos.org>
2026-08-16 16:33:38 -0400

    Update app_p4a.py

--- Problem 4a/app_p4a.py
+++ Problem 4a/app_p4a.py
@@ -4,7 +4,7 @@ def caesar_shift4(message):
   table = str.maketrans("abcdefghijklmnopqrstuvwxyz", "EFGHIJKLMNOPQRSTUVWXYZABCD")
   return message.translate(table)
 
-st.title("Problem 3 App")
+st.title("Problem 4a App")
 
 # Get user input
 user_message = st.text_input("Enter a plaintext message:")
